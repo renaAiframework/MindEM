a069cf1be5e451ce337aacd4f64bc1b7f431a3a2
Brain Lennox <burner@trangoinc.com>
2025-01-08 14:41:44 -0800

    Create AdversarialConfigs.py

---
+++ MindEM/domains/AdversarialConfigs.py
@@ -0,0 +1,4 @@
+from .code_generation import CodeAdversarialTester, CodeEvaluator
+from .code_prompt_writer import CodePromptWriter
+
+__all__ = ['CodeAdversarialTester', 'CodeEvaluator', 'CodePromptWriter'] 
